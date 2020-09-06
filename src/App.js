4ee945ac2147510d435fd75ac0810f50e2f30f11
fuston05 <fuston05@yahoo.com>
2020-09-05 21:01:27 -0400

    more toying with Ant Design

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import './App.less';
-import {Breadcrumb, Layout, Menu} from 'antd';
-const {Content, Header, Footer}= Layout;
+import { Breadcrumb, Layout, Menu } from 'antd';
+
+const { Content, Header, Footer } = Layout;
+
+const headerStyle= {
+  display: 'flex',
+  justifyContent: 'flex-start',
+  alignItems: 'center'
+}
 
 function App() {
   return (
-    <Layout style= {{height: '100vh'}} className= 'layout'>
-      <Header style= {{
-        display: 'flex',
-        justifyContent: 'flex-start',
-        alignItems: 'center'
-      }}>
-        <div className= 'logo'>Antd</div>
-        <Menu theme= 'dark' mode= 'horizontal' defaultSelectedKeys= {['1']}>
-          <Menu.Item key= '1'>Nav 1</Menu.Item>
-          <Menu.Item key= '2'>Nav 2</Menu.Item>
-          <Menu.Item key= '3'>Nav 3</Menu.Item>
+    <Layout style={{ height: '100vh' }} className='layout'>
+      <Header style={headerStyle}>
+        <div className='logo'>Antd</div>
+        <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['1']}>
+          <Menu.Item key='1'>Nav 1</Menu.Item>
+          <Menu.Item key='2'>Nav 2</Menu.Item>
+          <Menu.Item key='3'>Nav 3</Menu.Item>
         </Menu>
       </Header>
 
-      <Content style= {{padding: '0 50px'}}>
-        <Breadcrumb style= {{margin: '16px 0'}}>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
           <Breadcrumb.Item>Home</Breadcrumb.Item>
           <Breadcrumb.Item>List</Breadcrumb.Item>
           <Breadcrumb.Item>App</Breadcrumb.Item>
         </Breadcrumb>
-        <div className= 'site-layout-content'>Content</div>
+        <div className='site-layout-content'>Content</div>
       </Content>
 
-      <Footer style= {{textAlign: 'center'}}>Footer</Footer>
+      <Footer style={{ textAlign: 'center' }}>Footer</Footer>
     </Layout>
   );
 }
